dc7d1384c580dc745d53bcd9deec598d369c9160
Joshua Wootonn <joshuawootonn@gmail.com>
2023-07-29 22:00:28 -0500

    chore: update to include git in nvim-tree

--- init.lua
+++ init.lua
@@ -259,6 +259,17 @@ require("nvim-tree").setup {
     open_file = {
       quit_on_open = true,
     }
+  },
+  git = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
   }
   ---
 }
