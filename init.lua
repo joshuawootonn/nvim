7e6ac8c123ae3ad2dc80578f294cbc22a6ca5814
Joshua Wootonn <joshuawootonn@gmail.com>
2023-08-04 18:43:06 -0500

    revert: format on save

--- init.lua
+++ init.lua
@@ -315,18 +315,6 @@ null_ls.setup({
   },
   on_attach = function(client, bufnr)
     vim.cmd('map <Leader>lf :lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>')
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end
   end,
 })
 
